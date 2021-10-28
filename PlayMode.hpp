495d524ae73f8000e7e5edb38119b8de44fab3f3
gcwhitfield <gwhitfieldjr@gmail.com>
2021-10-28 12:11:45 -0400

    Resolve small compile error, pull from master

--- PlayMode.hpp
+++ PlayMode.hpp
@@ -30,12 +30,14 @@ struct PlayMode : Mode {
 
 	//player info:
 	struct Player {
+		Player() {}; // TODO: Implement this
+		~Player() {}; // TODO: Implement this
 		WalkPoint at;
 		//transform is at player's feet and will be yawed by mouse left/right motion:
 		Scene::Transform *transform = nullptr;
 		//camera is at player's head and will be pitched by mouse up/down motion:
 		Scene::Camera *camera = nullptr;
-		Scene::Drawable drawPlayer;
+		Scene::Drawable *drawPlayer;
 		enum Status {
 			Human, Cat
 		};
